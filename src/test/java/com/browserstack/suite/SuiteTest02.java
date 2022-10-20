cf6ab559b3aa9bf118e58e38804d878a96ebb5db
kamal-kaur04 <38219887+kamal-kaur04@users.noreply.github.com>
2022-10-20 14:55:17 +0530

    revert suite files

---
+++ src/test/java/com/browserstack/suite/SuiteTest02.java
@@ -0,0 +1,36 @@
+package com.browserstack.suite;
+
+import com.browserstack.BrowserStackTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SuiteTest02 extends BrowserStackTest {
+
+    @Test
+    public void test() throws Exception {
+
+        open("https://www.bstackdemo.com");
+        sleep(2000);
+
+        String selectedProduct = $(By.xpath("//*[@id=\"1\"]/p")).text();
+        $(By.xpath("//*[@id=\"1\"]/div[4]")).click();
+        sleep(2000);
+
+        // waiting for cart to load
+        while(!$(".float-cart__content").isDisplayed()) {
+            sleep(1000);
+        }
+
+        String productInCart = $(
+            By.xpath(
+                "//*[@id=\"__next\"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]"
+            )
+        ).text();
+
+        // assert the product clicked has been added to cart
+        Assert.assertEquals(selectedProduct, productInCart);
+    }
+}
