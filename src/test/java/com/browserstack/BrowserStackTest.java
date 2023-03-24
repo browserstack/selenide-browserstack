c1348a3c2ac703036ae9c1e1b2493ef214a1cd48
Neha Agarwal <agarneha1331@gmail.com>
2023-03-24 15:20:09 +0530

    Fix for parallel with local

--- src/test/java/com/browserstack/BrowserStackTest.java
+++ src/test/java/com/browserstack/BrowserStackTest.java
@@ -14,74 +14,90 @@ import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.*;
 import org.testng.annotations.Parameters;
 
 import com.codeborne.selenide.WebDriverRunner;
 
 
 public class BrowserStackTest {
-	public RemoteWebDriver driver;
-	private Local l;
+    public RemoteWebDriver driver;
+    private Local l;
+    private static JSONObject config;
+    private static Map<String, Object> commonCapabilities;
+    public static String username;
+    public static String accessKey;
+    public static String sessionId;
 
-	public static String username;
-	public static String accessKey;
-	public static String sessionId;
+    @BeforeSuite(alwaysRun = true)
+    @Parameters(value = { "config"})
+    public void beforeSuite(String config_file) throws Exception {
+        JSONParser parser = new JSONParser();
+        config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
+        commonCapabilities = (Map<String, Object>) config.get("capabilities");
+        
+        username = System.getenv("BROWSERSTACK_USERNAME");
+        if (username == null) {
+            username = (String) config.get("user");
+        }
 
-	@BeforeMethod(alwaysRun=true)
-	@Parameters(value={"config", "environment"})
-	public void setUp(String config_file, String environment) throws Exception {
-		JSONParser parser = new JSONParser();
-		JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
-		JSONObject envs = (JSONObject) config.get("environments");
+        accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        if (accessKey == null) {
+            accessKey = (String) config.get("key");
+        }
+        try {
+            if ((commonCapabilities.get("browserstack.local") != null &&
+            commonCapabilities.get("browserstack.local").toString().equalsIgnoreCase("true") && (l == null))) {
+                l = new Local();
+                Map<String, String> options = new HashMap<String, String>();
+                options.put("key", accessKey);
+                l.start(options);
+            }
+        } catch (Exception e) {
+            System.out.println("Error while start local - " + e);
+        }
+    }
 
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("browserstack.source","selenide:sample-master:v1.0");
+    @BeforeMethod(alwaysRun = true)
+    @Parameters(value = { "config", "environment" })
+    public void setUp(String config_file, String environment) throws Exception {
+        JSONParser parser = new JSONParser();
+        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
+        JSONObject envs = (JSONObject) config.get("environments");
 
-		Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
-		Iterator it = envCapabilities.entrySet().iterator();
-    	while (it.hasNext()) {
-      		Map.Entry pair = (Map.Entry)it.next();
-      		capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-    	}
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserstack.source","selenide:sample-master:v1.0");
 
-		Map<String, String> commonCapabilities= (Map<String, String>) config.get("capabilities");
-		it = commonCapabilities.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry pair = (Map.Entry)it.next();
-			if (capabilities.getCapability(pair.getKey().toString()) == null) {
-				capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-			}
-		}
+        Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
+        Iterator it = envCapabilities.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+        }
 
-		username = System.getenv("BROWSERSTACK_USERNAME");
-		if (username == null) {
-			username = (String) config.get("user");
-		}
+        Map<String, String> commonCapabilities= (Map<String, String>) config.get("capabilities");
+        it = commonCapabilities.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            if (capabilities.getCapability(pair.getKey().toString()) == null) {
+                capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+            }
+        }
 
-		accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-		if (accessKey == null) {
-			accessKey = (String) config.get("key");
-		}
+        driver = new RemoteWebDriver(new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        sessionId = driver.getSessionId().toString();
 
-		if (capabilities.getCapability("browserstack.local") != null && capabilities.getCapability("browserstack.local") == "true") {
-			l = new Local();
-			Map<String, String> options = new HashMap<String, String>();
-			options.put("key", accessKey);
-			l.start(options);
-		}
+        WebDriverRunner.setWebDriver(driver);
+    }
 
-		driver = new RemoteWebDriver(new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		sessionId = driver.getSessionId().toString();
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
 
-		WebDriverRunner.setWebDriver(driver);
-	}
-
-	@AfterMethod(alwaysRun=true)
-	public void tearDown() throws Exception {
-		WebDriverRunner.closeWebDriver();
-		if (l != null) l.stop();
-	}
+    @AfterSuite(alwaysRun = true)
+    public void afterSuite() throws Exception {
+        if (l != null) l.stop();
+    }
 }
