1ac63c75980f08cd04910bf95b5202e3e96ef3c1
Neha Agarwal <agarneha1331@gmail.com>
2023-06-23 17:17:36 +0530

    moved source

--- src/test/java/com/browserstack/BrowserStackTest.java
+++ src/test/java/com/browserstack/BrowserStackTest.java
@@ -35,6 +35,7 @@ public class BrowserStackTest {
     public void setUp() throws Exception {
         MutableCapabilities capabilities = new MutableCapabilities();
         HashMap<String, String> bstackOptions = new HashMap<>();
+        bstackOptions.put("source", "selenide:sample-master:v1.2");
         capabilities.setCapability("bstack:options", bstackOptions);
         driver = new RemoteWebDriver(new URL(String.format("https://%s:%s@hub-cloud.browserstack.com/wd/hub", userName, accessKey)), capabilities);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
